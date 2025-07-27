f7879deefd8a9a79cd15d4324c40a460aa0c448f
saramozun <saramozun@gmail.com>
2025-07-26 23:02:46 -0600

    Update README, app.py, requirements.txt, vehicles_us.csv, EDA.ipynb

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # project_sprint7
 Repositorio para el proyecto del desarrollo de una aplicación web
+
+## Descripción del proyecto
+El objetivo de este proyecto 7 se centra en usar nuestras habilidades de ingeniería de software aprendidas en el sprint 7, para poder crear una aplicación web donde se muestra un Data Frame de un conjunto de datos sobre vehículos, seguido de un histograma y una gráfica de dispersión.
+Las tareas incluyen la creación y gestión de entornos virtuales de Python y el desarrollo de una aplicación web.
---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+car_data = pd.read_csv('/Users/saram/Notebooks/project_sprint7/vehicles_us.csv') # leer los datos
+st.header('Data viewer: Vehicles')
+hist_button = st.button('Construir histograma')
+if hist_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+                               
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+                                                      
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button('Construir diagrama de dispersión')
+if scatter_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un diagrama de dispersión
+    fig = px.scatter(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
